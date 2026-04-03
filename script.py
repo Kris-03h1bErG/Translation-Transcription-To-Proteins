b2869e441594af3f16e1c684bccbfcd230f2d1e0
Kris-03h1bErG <s1007469@student.knoxschools.org>
2026-04-03 23:45:31 +0000

    Comeleted the Protein synthisis dict and Convert RNA to Protein strand dict

--- script.py
+++ script.py
@@ -1,6 +1,76 @@
 # to convert DNA to Protein Strains 
 # Note to self try to optimize code as much as possible 
 
+protein_synthisis = {
+    "CU" : "Leu",
+    "GU" : "Val",
+    "UC" : "Ser",
+    "CC" : "Pro",
+    "AC" : "Thr",
+    "GC" : "Ala",
+    "CG" : "Arg",
+    "GG" : "Gly",
+    "2" : 
+    {                  #Looking for last letter just a shorter way to make 2 keys asoiated to one value
+        "UU" : 
+        {
+            "U" : "Phe",
+            "C" : "Phe",
+            "A" : "Leu",
+            "G" : "Leu"
+        },         
+        "UA" : 
+        {
+            "U" : "Tyr",
+            "C" : "Tyr",
+            "A" : "Stop",
+            "G" : "Stop"
+        },  
+        "CA" : 
+        {
+            "U" : "His",
+            "C" : "His",
+            "A" : "Gln",
+            "G" : "Gln"
+        },  
+        "AA" : 
+        {
+            "U" : "Asn",
+            "C" : "Asn",
+            "A" : "Lys",
+            "G" : "Lys"
+        },  
+        "GA" : 
+        {
+            "U" : "Asp",
+            "C" : "Asp",
+            "A" : "Glu",
+            "G" : "Glu"
+        },  
+        "UG" : 
+        {
+            "U" : "Cys",
+            "C" : "Cys",
+            "A" : "Stop",
+            "G" : "Trp"
+        },  
+        "AG" : 
+        {
+            "U" : "Ser",
+            "C" : "Ser",
+            "A" : "Arg",
+            "G" : "Arg"
+        },  
+        "AU" : 
+        {
+            "U" : "Ile",
+            "C" : "Ile",
+            "A" : "Ile",
+            "G" : "Met"
+        },  
+    }
+}
+
 def getDnaStrand():
     return input("Please enter DNA with spaces seperating each Codon: ")
 
@@ -19,26 +89,27 @@ def getDnaAndConvertToRNA():
         new_Codon_Strand.append(new_Codon)
     #print(new_Codon_Strand)
     return new_Codon_Strand
-getDnaAndConvertToRNA()
-def convertRNAToProtein(rna_Codon_strand):
+#getDnaAndConvertToRNA()
+def convertRNAToProteinStrand(rna_Codon_strand):
+    polypeptide_strand = []
     for codon in rna_Codon_strand:
-        for nucleotide in codon:
-            # Logic here is to check the least amount of datta possible 4-2-3-1
-            # 4's only need to check first 2 in Codon 2's need all 3 but is still 1 check but have to check 3 letters than 1 or 2
-            # 3's and 1's are shown 1 and 3 times with 3 preffered as it has a greater chance and less data than 1's 
-            protein_synthisis = {
-                "CU" : "Leu",
-                "GU" : "Val",
-                "UC" : "Ser",
-                "CC" : "Pro",
-                "AC" : "Thr",
-                "GC" : "Ala",
-                "CG" : "Arg",
-                "GG" : "Gly",
-                "2" : 
-                {                  #Looking for last letter 
-                    "UU" : dict.fromkeys(["U", "C"], "Phe"),              
-                }
+        if len(codon) == 3:
+                first_two = codon[0] + codon[1]
+                resulting_protein = protein_synthisis.get(first_two)
+                if resulting_protein != None:
+                    polypeptide_strand.append(resulting_protein)
+                else:
+                    last_one = codon[2] 
+                    try:
+                        protein = protein_synthisis["2"][first_two].get(last_one)
+                        polypeptide_strand.append(protein)
+                    except:
+                        protein = "Error"
+                        polypeptide_strand.append(protein)
+        else:
+            protein = "Error"
+            polypeptide_strand.append(protein)
             
-
-            }
+        
+    return polypeptide_strand
+print(convertRNAToProteinStrand(getDnaAndConvertToRNA()))
